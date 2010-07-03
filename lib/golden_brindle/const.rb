33aa3dbc6d78844f7025c1a60af0b539abd78c19
Alexander Simonov <alex@simonov.in.ua>
2010-07-03 16:43:50 +0300

    version bump

--- lib/golden_brindle/const.rb
+++ lib/golden_brindle/const.rb
@@ -3,7 +3,7 @@ module GoldenBrindle
   module Const
     
     # current version
-    VERSION="0.0.4".freeze
+    VERSION="0.0.5".freeze
     # main banner
     BANNER = "Usage: golden_brindle <command> [options]".freeze
     
