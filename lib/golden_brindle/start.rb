f724388b46a8ddc5c6152a9d31813c2e13b55cb4
Alexander Simonov <alex@simonov.me>
2011-09-03 17:53:26 +0300

    Code cleanup

--- lib/golden_brindle/start.rb
+++ lib/golden_brindle/start.rb
@@ -1,8 +1,8 @@
-
 module Brindle
   
   class Start < GemPlugin::Plugin "/commands"
     include GoldenBrindle::Command::Base
+    include Hooks
     
     def configure
       options [
@@ -56,20 +56,9 @@ module Brindle
       valid_group? @group if @group
       @valid
     end
-    
-    def run
-      # Change there to start, then we'll have to come back after daemonize
-      Dir.chdir(@cwd)
-      if @bundler
-        puts "Using Bundler"
-        puts "reexec via bundle exec"
-        cmd_for_exec = "bundle exec #{@opt.program_name}"
-        @original_args.each_slice(2) do |arg_key,value|
-          cmd_for_exec << " #{arg_key} #{value}" if arg_key != "-b"
-        end
-        exec(cmd_for_exec)
-      end
-      options = { 
+
+    def default_options
+      {
         :listeners          => [],
         :pid                => @pid_file,
         :config_file        => @config_script,
@@ -77,72 +66,52 @@ module Brindle
         :working_directory  => @cwd,
         :timeout            => @timeout.to_i
       }
+    end
+
+    def bundler_cmd
+      cmd = "bundle exec #{@opt.program_name}"
+      @original_args.each_slice(2) do |arg_key,value|
+        cmd << " #{arg_key} #{value}" if arg_key != "-b"
+      end
+      cmd
+    end
+
+    def collect_listeners
+      return if @port.nil?
+      start_port = end_port = nil
+      start_port ||=  @port.to_i
+      end_port ||=  start_port + @servers.to_i - 1
+      (start_port..end_port).map do |port|
+        "#{@address}:#{port}"
+      end
+    end
+
+    def parse_listen_option
+      return if @listen.nil? || @listen.empty?
+      @listen.split(',').map do |listen|
+        listen = File.join(@cwd,listen) if listen[0..0] != "/" && !listen.match(/\w+\:\w+/)
+        "#{listen}"
+      end
+    end
+    
+    def run
+      # Change there to start, then we'll have to come back after daemonize
+      Dir.chdir(@cwd)
+      if @bundler
+        puts "Using Bundler"
+        puts "reexec via bundle exec"
+        exec(bundler_cmd)
+      end
+      options = default_options
       # set user via Unicorn options. If we don't set group - then use only user
       options[:user] = @user unless @user.nil? 
       options[:stderr_path] = options[:stdout_path] = @log_file if @daemon
-      # ensure Rails standard tmp paths exist
-      options[:after_reload] = lambda do
-        FileUtils.mkdir_p(%w(cache pids sessions sockets).map! { |d| "tmp/#{d}" })
-      end
-        
       options[:preload_app] = @preload
-      # do base steps for Rails
-      options[:after_fork] = lambda do |server, worker|
-        defined?(ActiveRecord::Base) and
-          ActiveRecord::Base.establish_connection
-          # trying to change user and group
-        begin
-          # check if something not set in config or cli
-          unless @user.nil? || @group.nil?
-            uid, gid = Process.euid, Process.egid
-            user, group = @user, @group
-            target_uid = Etc.getpwnam(user).uid
-            target_gid = Etc.getgrnam(group).gid
-            worker.tmp.chown(target_uid, target_gid)
-            if uid != target_uid || gid != target_gid
-              Process.initgroups(user, target_gid)
-              Process::GID.change_privilege(target_gid)
-              Process::UID.change_privilege(target_uid)
-            end
-          end
-         rescue => e
-           if ENV['RAILS_ENV'] == 'development'
-             STDERR.puts "couldn't change user, oh well"
-           else
-             raise e
-           end
-         end
-      end   
-      options[:before_fork] = lambda do |server, worker| 
-        defined?(ActiveRecord::Base) and
-          ActiveRecord::Base.connection.disconnect!
-        # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-        if GC.respond_to?(:copy_on_write_friendly=)
-            GC.copy_on_write_friendly = true
-        end
-        old_pid = "#{server.config[:pid]}.oldbin"
-        if File.exists?(old_pid) && server.pid != old_pid
-          begin
-            Process.kill("QUIT", File.read(old_pid).to_i)
-          rescue Errno::ENOENT, Errno::ESRCH
-          end
-        end
-      end
+      options.merge!(collect_hooks)
       ENV['RAILS_ENV'] = @environment
       ENV['RAILS_RELATIVE_URL_ROOT'] = @prefix
-      unless @port.nil?
-        start_port = end_port = nil
-        start_port ||=  @port.to_i
-        end_port ||=  start_port + @servers.to_i - 1
-        (start_port..end_port).each do |port|
-          options[:listeners] << "#{@address}:#{port}"
-        end
-      end
-      unless @listen.nil? || @listen.empty?
-        @listen.split(',').each do |listen|
-          listen = File.join(@cwd,listen) if listen[0..0] != "/" && !listen.match(/\w+\:\w+/)
-          options[:listeners] << "#{listen}"
-        end
+      [collect_listeners, parse_listen_option].each do |listeners|
+        options[:listeners] += listeners if listeners
       end
       app = RailsSupport.rails_builder(@daemon)
       if @daemon
